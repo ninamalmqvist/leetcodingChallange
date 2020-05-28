286888a0186cc761e85fd2a7bcc96fc2b4a721cb
Nina <ninamal@student.chalmer.se>
2020-05-28 23:25:57 +0200

    May28FinalVersion

---
+++ CountBinary.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class CountBinary {
+
+    public static void main(String args[]){
+        Solution solution=new Solution();
+        System.out.println(Arrays.toString(solution.countBits(10)));
+    }
+}
+;
